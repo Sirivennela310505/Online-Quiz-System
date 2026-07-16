6cf242753dce58731899eebc8ff02bfc0d6a9bb8
Sirivennela310505 <your-email@gmail.com>
2026-07-16 09:42:37 +0530

    first commit

---
+++ OQSbackend/OQSbackend/src/main/java/com/mru/OQSbackend/OqSbackendApplication.java
@@ -0,0 +1,13 @@
+package com.mru.OQSbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqSbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OqSbackendApplication.class, args);
+	}
+
+}
---
+++ OQSbackend/OQSbackend/src/test/java/com/mru/OQSbackend/OqSbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mru.OQSbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OqSbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
